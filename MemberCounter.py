e9fd51b7a13dbf51b8c0ba835e953d53e197aa53
luojie <744240250@qq.com>
2018-05-14 20:36:21 +0800

    更新内容

--- MemberCounter.py
+++ MemberCounter.py
@@ -1,5 +1,6 @@
+#_*_coding:utf-8_*_
 __metaclass__ = type
-
+import shelve
 
 class MemberCounter:
     members = 0
@@ -24,13 +25,33 @@ class StudentMember(MemberCounter, Person):
         print 'hello world'
     pass
 
+def testShelve():
+    s = shelve.open('test.dat')
+    #类似于字典一样的东东。可以存储对象。
+    s['x'] = ['a', 'b', 'c']
+    temp = s['x'] #取出x对应的值
+    print s
+    temp.append('d') #给数组添加元素
+    s['x'] = temp #更新数据库中的信息。
+    print s
 
 def main():
     memberOne = MemberCounter()
     print memberOne.members
     student = StudentMember()
+    studentDB = shelve.open('test.dat')
+    studentDB['A'] = student
+    print studentDB
+    studenttest = studentDB['A']
+    studenttest.functest()
     print hasattr(student, 'functest')
     print callable(getattr(student, 'functest', None))
+    from constants import test
+    test.test()
+    print dir(test)
+    listS = [0,1,2,3,1,2,3,4,5,6,7,8,9,7,4,5,6]
+    print set(listS)
+    testShelve()
     pass
 if __name__ == '__main__':
     main()
